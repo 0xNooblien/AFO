027b878247b0d8f705a6ebbd57adcec679481b55
0xNooblien <rsm050501@gmail.com>
2022-11-05 16:35:19 +0530

    AFO 0.0.1

---
+++ afo.js
@@ -0,0 +1,7 @@
+// import chalk from 'chalk';
+// import colors from "colors";
+var colors = require('colors');
+// console.log(chalk.blue('Hello world!'));
+console.log('AFOOOOOOOO'.rainbow);
+console.log('Now Your Code will not give any weird error'.rainbow);
+console.log('Regards ANTS'.rainbow);
